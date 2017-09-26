6bc7191f978ed6ad1f1a9f48a1cbc8884157656c
Lauren Anderson <laurenra@byu.edu>
2017-09-26 16:04:39 -0600

    Initial project files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple Spring MVC web application
+This can be used as a template for building a Spring MVC web application.
+
+## Build
+Use Maven to build.
+
+```
+mvn clean package
+```
+
+Copy the springwebapp.war file to the Tomcat /webapps directory to deploy it.
---
+++ webapp/src/main/java/org/la/sample/webapp/controllers/MainController.java
@@ -0,0 +1,30 @@
+package org.la.sample.webapp.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by laurenra on 9/14/17.
+ */
+@Controller
+public class MainController {
+
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView showIndex(HttpServletRequest request, HttpServletResponse response) {
+
+        log.info("Show index page...");
+
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
+}
